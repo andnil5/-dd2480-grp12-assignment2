63848c6781096262b4ed68fb505677d783ff91fc
Jennifer Lindberg <jennifel@kth.se>
2021-02-10 17:41:08 +0100

    feat: add api connection to edit commit status, issue #6

--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import ci_utils
+from status_response import Status_response, StatusType
 
 
 app = Flask(__name__)
@@ -12,6 +13,10 @@ def webhook():
         data = ci_utils.parse(request.json)
         if 'error' in data:
             abort(400)
+        # Set commit compile status flag to pending
+        Status_response(10, StatusType.compile, data['head_commit'], '').send_status()
+        # Set commit test status flag to pending
+        Status_response(10, StatusType.test, data['head_commit'], '').send_status()
         return 'success', 200
     else:
         abort(400)
