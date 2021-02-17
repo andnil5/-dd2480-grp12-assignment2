717e282bdbadd0ab0cc4961b73ba9ff173eb89e3
Omid <omidhazara@gmail.com>
2021-02-17 18:59:36 +0100

    testing one more time

--- tests/test_ci_utils.py
+++ tests/test_ci_utils.py
@@ -10,15 +10,5 @@ def test_parse_correct_keys():
         }
     }
     res = parse(data)
-    assert res['branch'] == 'ci_serr'
+    assert res['branch'] == 'ci_server'
     assert res['head_commit'] == '3f28d0dd76b9b1bdd5db5224a1012e5738d2b7b5'
-
-
-def test_parse_missing_keys():
-    """Test that parse sets error if key does not exist."""
-    data = {}
-    res = parse(data)
-
-    assert 'ref' not in res
-    assert 'head_commit' not in res
-    assert res['error'] == 'Missing key'
